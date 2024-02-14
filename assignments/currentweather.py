820bbed6dabdd79ad9ef21108a371950b2bf8b72
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-14 18:52:32 +0000

    shorter script

--- assignments/currentweather.py
+++ assignments/currentweather.py
@@ -4,7 +4,6 @@
 # author: Nataliia Dragunova
 
 # current temperature in Kyiv(Ukraine)
-
 import requests
 import json
 
@@ -12,11 +11,8 @@ url = "https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238
 response = requests.get(url)
 # print (response.json())
 data = response.json()
-# with open ("v1dump.json") as fp:
-# json.dump(data)
 
-current = data["current"]
-temp = current["temperature_2m"]
+temp = data["current"]["temperature_2m"]
 # print(temp)
 print(temp)
 
@@ -28,10 +24,7 @@ url = "https://api.open-meteo.com/v1/forecast?latitude=50.4547&longitude=30.5238
 response = requests.get(url)
 # print (response.json())
 data = response.json()
-# with open ("v1dump.json") as fp:
-# json.dump(data)
 
-current = data["current"]
-wind = current["wind_direction_10m"]
+wind = data["current"]["wind_direction_10m"]
 # print(wind)
 print(wind)
